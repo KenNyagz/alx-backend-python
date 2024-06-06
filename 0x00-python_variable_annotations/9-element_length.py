ead7f85290e42a6d202e2fa2a29bf23d3e9c2746
KenNyagz <kennyagz681@gmail.com>
2024-06-06 22:42:14 +0300

    Complex Duck type

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 '''
-
+type annotation for a function that takes in a list of sequnces
+and returns a list of tuples with a sequence and an int each
 '''
 from typing import List, Tuple, Sequence, Iterable
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''returns list of tuples with a sequence and the length of it'''
     return [(i, len(i)) for i in lst]
