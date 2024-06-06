732c9e9f3e3e3bef51bde327e115c10f78fe92e5
KenNyagz <kennyagz681@gmail.com>
2024-06-06 23:36:17 +0300

    TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,10 +3,11 @@
 Duck type annotation for function taking in a dictionary-like object
 '''
 from typing import Mapping, Any, Union, TypeVar
-
-
 T = TypeVar('T')
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]  = None) -> Union[Any, T]:
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     ''' return dictionary key or something else'''
     if key in dct:
         return dct[key]
