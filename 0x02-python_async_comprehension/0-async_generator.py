59ab4464488f7056c18706981ebe48217ae8f95c
KenNyagz <kennyagz681@gmail.com>
2024-06-12 09:32:28 +0300

    aliased commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ each the yield a custom number
 '''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     ''' oops 10 times waiting for 1 sec each the yield a custom numbe'''
     for _ in range(10):
         await asyncio.sleep(1)
