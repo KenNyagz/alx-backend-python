bc30e21d89347772b6ac94a12db3b9c49cbda129
KenNyagz <kennyagz681@gmail.com>
2024-07-01 17:11:12 +0300

    Parameterize a unit test with exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,6 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+     ({'a': 'str'}, ('b',)),
+     ({'a': {'b': 'str'}}, ('a', 'c'))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        '''test exception raised by wrong input fed into function'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 if __name__ == '__main__':
     unittest.main()
